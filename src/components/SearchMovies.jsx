05c081869663fe5db921d700ecef92023860eb24
Prudence Codjia <pcodjia@ironguard.net>
2020-05-30 15:45:43 -0400

    Using apis tp tap in themovieSearchDatabase

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -2,10 +2,26 @@ import  React  from 'react';
 
 
 function SearchMovies (){
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("Submitting")
+        const query = "Django";
+
+        const url = `https://api.themoviedb.org/3/movie/550?api_key=9a621bceb7d36f9a48b31e81bcfe6ddb
+&language=en-US&query=${query}&page=1&include_adult=false`;
+
+       try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+       }catch(err){
+            console.log(err)
+       }
+    }
     return(
         
-            <form className="form">
-                    <label className="label" htmlFor="query">Movies Name</label>
+            <form className="form" onSubmit={searchMovies}>
+                    <label className="label" htmlFor="query">Movie Name</label>
                     <input className="input" type="text" name="query" placeholder="i.e. Django"/>
             <button className="button" type="submit">Search</button>
             </form>
